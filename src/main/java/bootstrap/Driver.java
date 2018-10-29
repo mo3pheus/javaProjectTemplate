dc87ff49a9b3d89ff1918e3e25bb7ec1ac6a6db1
Sanket <sanket.korgaonkar@gmail.com>
2018-10-29 14:55:45 -0400

    Feature: Removed sound project specific code and added test dependency.

--- src/main/java/bootstrap/Driver.java
+++ src/main/java/bootstrap/Driver.java
@@ -1,10 +1,8 @@
 package bootstrap;
 
-import etl.SoundMetaData;
 import org.apache.log4j.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import utils.FileUtil;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -15,29 +13,15 @@ public class Driver {
             "==============================================================";
 
     public static Properties projectProperties = new Properties();
-    public static Logger logger = LoggerFactory.getLogger(Driver.class);
+    public static Logger     logger            = LoggerFactory.getLogger(Driver.class);
 
     public static void main(String[] args) {
         try {
             String logFilePath = "";
-            configureConsoleLogging(Boolean.parseBoolean(args[0]));
+            configureLogging(Boolean.parseBoolean(args[0]));
             logger.info(SEPARATOR);
-            logger.info(" Project properties are loaded. Log file generated for this run = " + logFilePath);
+            logger.info("Project properties are loaded. Log file generated for this run = " + logFilePath);
             projectProperties = getProjectProperties(args[1]);
-
-            SoundMetaData soundMetaData = new SoundMetaData();
-            soundMetaData.initializeLabelledData(projectProperties.getProperty("audio.resources.path"));
-            soundMetaData.getManuallyVerifiedFiles(projectProperties.getProperty("audio.resources.train.metaFile"));
-
-            logger.info(SEPARATOR);
-            for (String uniqueClass : soundMetaData.getManuallyVerifiedClasses()) {
-                logger.info(uniqueClass);
-            }
-            logger.info("Total number of manually verified classes = " + soundMetaData.getManuallyVerifiedClasses().size());
-            logger.info(SEPARATOR);
-
-            FileUtil.writeCompDataToDisk(soundMetaData.getClassCompositionGrndTruth(),projectProperties.getProperty("audio.resources.class.compositionFile"));
-
         } catch (IOException io) {
             logger.error("Error while reading the project properties file.", io);
         }
@@ -46,7 +30,7 @@ public class Driver {
     public static String configureLogging(boolean debug) {
         FileAppender fa = new FileAppender();
 
-        if (!debug) {
+        if ( !debug ) {
             fa.setThreshold(Level.toLevel(Priority.INFO_INT));
             fa.setFile("executionLogs/log_infoLevel_report_" + Long.toString(System.currentTimeMillis()) + ".log");
         } else {
@@ -63,7 +47,7 @@ public class Driver {
 
     public static void configureConsoleLogging(boolean debug) {
         ConsoleAppender ca = new ConsoleAppender();
-        if (!debug) {
+        if ( !debug ) {
             ca.setThreshold(Level.toLevel(Priority.INFO_INT));
         } else {
             ca.setThreshold(Level.toLevel(Priority.DEBUG_INT));
@@ -75,8 +59,8 @@ public class Driver {
 
     public static Properties getProjectProperties(String propertiesFilePath) throws IOException {
         logger.info("Properties file specified at location = " + propertiesFilePath);
-        FileInputStream projFile = new FileInputStream(propertiesFilePath);
-        Properties properties = new Properties();
+        FileInputStream projFile   = new FileInputStream(propertiesFilePath);
+        Properties      properties = new Properties();
         properties.load(projFile);
         return properties;
     }
